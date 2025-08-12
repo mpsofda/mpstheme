3318d7ba2d4783f53ae00f5c49dc1e3fc201095c
mgmhughes <150265034+mgmhughes@users.noreply.github.com>
2025-08-12 10:53:24 -0500

    Package updates; callout boxes
    Added callout box functions for both LaTeX and HTML documents

--- R/mpstheme-package.R
+++ R/mpstheme-package.R
@@ -5,6 +5,9 @@
 #' @import ggplot2
 #' @importFrom ggfun element_roundrect
 #' @importFrom grDevices colorRampPalette
+#' @importFrom htmltools br
+#' @importFrom htmltools div
+#' @importFrom htmltools span
 #' @importFrom showtext showtext_auto
 #' @importFrom sysfonts font_add
 ## usethis namespace: end
